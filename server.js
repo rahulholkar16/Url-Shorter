4da158f34ba7fefa363cb45d4ba07d964bafbbf1
Rahul Pal <usefulltech1819@gmail.com>
2025-08-24 14:52:18 +0530

    fix get bug

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use('/', express.static(path.join(__dirname, 'client')));
 
 // ----- Route -----
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'index.html'));
 })
 
