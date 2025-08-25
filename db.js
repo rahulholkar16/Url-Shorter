b1ee4f3671193150a89356316db78769db970ce9
Rahul Pal <usefulltech1819@gmail.com>
2025-08-25 16:53:15 +0530

    Add user Schema

--- db.js
+++ db.js
@@ -2,6 +2,13 @@ import { mongoose } from "./utils/ImortExport.js";
 
 const Schema = mongoose.Schema;
 
+const UserSchema = new Schema({
+    "name": { type: String, required: true },
+    "email": { type: String, unique: true, required: true },
+    "password": { type: String, required: true },
+    "userHistory": [{ type: Schema.Types.ObjectId, ref: "history" }]
+})
+
 const UrlSchema = new Schema({
     "origenalUrl": { type: String,  required: true },
     "sortUrl": { type: String, unique: true },
