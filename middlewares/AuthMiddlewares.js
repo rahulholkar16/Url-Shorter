16581b8c0753b345e8c1660de510906b2ee9c770
Rahul Pal <usefulltech1819@gmail.com>
2025-08-25 17:33:37 +0530

    Add Auth middleware

---
+++ middlewares/AuthMiddlewares.js
@@ -0,0 +1,27 @@
+import { jwt } from "../utils/ImortExport.js";
+
+export async function auth(req, res, next) {
+    try {
+        const token = req.cookies?.token;
+        if (!token) return res.status(401).json({ msg: "No token provide!" });
+
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+        if (!decode) return res.status(401).json({ msg: "Invalid User!" });
+
+        const user = await UserModel.findById(decode.id).select("-password"); // exclude password
+        if (!user) {
+            return res.status(401).json({ msg: "Unauthorized: User not found" });
+        }
+
+        req.userId = decode.id;
+        next();
+    } catch (error) {
+        console.error("Auth Middleware Error:", error.message);
+
+        if (error.name === "TokenExpiredError") {
+            return res.status(401).json({ msg: "Unauthorized: Token expired" });
+        }
+
+        return res.status(401).json({ msg: "Unauthorized: Invalid token" });
+    }
+}
