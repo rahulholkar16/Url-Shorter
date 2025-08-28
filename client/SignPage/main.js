1011948c51cf9b4fc4720ce93edc4ff512abacdb
Rahul Pal <usefulltech1819@gmail.com>
2025-08-28 23:40:23 +0530

    add Signup login client side

---
+++ client/SignPage/main.js
@@ -0,0 +1,47 @@
+const SignUpBtn = document.querySelector('.signup');
+const LoginBtn = document.querySelector('.login');
+const NavLoginBtn = document.querySelector('.NavLogin');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+
+SignUpBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+    
+    const data = {
+        name: name.value,
+        email: email.value,
+        password: password.value
+    }
+    
+    try {
+        const response = await fetch('http://localhost:3000/api/v1/signup', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+        console.log(result);
+
+        if (result.msg === "User added Successfully!") {
+            console.log("Hello");
+
+            window.location.href = "../LoginPage/index.html";
+
+        } else {
+            alert(result.msg);
+        }
+    } catch (error) {
+        console.error(err);
+        alert("Something went wrong!");
+    }
+});
+
+LoginBtn.addEventListener("click", () => {
+    window.location.href = "../LoginPage/index.html";
+});
+
+NavLoginBtn.addEventListener("click", () => {
+    window.location.href = "../LoginPage/index.html";
+});
