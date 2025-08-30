d60b0a615db1e7a4e59db497892df5ab33802606
Rahul Pal <usefulltech1819@gmail.com>
2025-08-31 00:01:31 +0530

    fix some bug

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/", express.static(path.join(__dirname, '../Frontend')));
+app.use("/", express.static(path.join(__dirname, '../public')));
 
 // ----- Route -----
 // app.get('/', (req, res) => {
